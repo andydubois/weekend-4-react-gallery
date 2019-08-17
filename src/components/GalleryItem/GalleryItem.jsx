90c32b422cf40e39fcaaec4499de70301b99e2dd
Andy DuBois <andydubois89@gmail.com>
2019-08-16 21:37:00 -0500

    forgot to commit on branch Gallery Item, brought over changes to master.  GalleryItem renders on DOM, when click description shows, but DIV moves downward for some reason

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,13 +1,41 @@
-import React, {Component} from 'react';
-import galleryItems from '../../../server/routes/gallery.router'
+import React, { Component } from "react";
 
 class GalleryItem extends Component {
-    render() {
-        let pictures = {this.props.galleryItems};
-        return(
-            {pictures};
-        )
+  state = {
+    pictureShowing: true
+  };
+
+  clickPicture = () => {
+    console.log("picture clicked");
+    this.setState({
+      pictureShowing: !this.state.pictureShowing
+    });
+  };
+
+  flipPicture = () => {
+    if (this.state.pictureShowing === true) {
+      return (
+        <div className='pictureDiv'>
+          <img
+            onClick={this.clickPicture}
+            src={this.props.picture.path}
+            alt=''
+            className='pictureBox'
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className='descriptionDiv' onClick={this.clickPicture}>
+          <p className='textBox'>{this.props.picture.description}</p>
+        </div>
+      );
     }
+  };
+
+  render() {
+    return <>{this.flipPicture()}</>;
+  }
 }
 
 export default GalleryItem;
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,21 +1,16 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import GalleryItem from "../GalleryItem/GalleryItem";
 
 class GalleryList extends Component {
-    render() {
+  render() {
+    let pictureGallery = this.props.listOfPictures.map(picture => {
+      return <GalleryItem picture={picture} />;
+      //code to use in GalleryItem component
+      // <img src={picture.path} alt='' className='pictureBox'></img>
+    });
 
-        let pictureGallery = this.props.listOfPictures.map(picture => {
-            return <img src={picture.path} alt='' className='pictureBox'></img>
-        })
-
-        return(
-         <>   
-         {pictureGallery}
-         </>
-        )
-
-    }
+    return <>{pictureGallery}</>;
+  }
 }
-
 
 export default GalleryList;
